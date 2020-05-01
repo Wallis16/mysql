a832f489f776cad6dc65422996e51def7e503643
Diógenes Wallis de França Silva <diogeneswallis@outlook.com>
2020-04-30 23:50:47 -0300

    Update movies.sql

--- questions/movies.sql
+++ questions/movies.sql
@@ -21,3 +21,29 @@ SELECT * FROM filmes ORDER BY nota_especialistas ASC LIMIT 10;
 SELECT * FROM filmes WHERE custo = (SELECT MAX(custo) FROM filmes);
 SELECT * FROM filmes WHERE custo = (SELECT MIN(custo) FROM filmes);
 
+#q6: mean from professionals rate
+#ans6
+SELECT AVG(nota_especialistas) FROM filmes;
+
+#q7: mean from normal people rate
+#ans7
+SELECT AVG(nota_audiencia) FROM filmes;
+
+#q8: mean from movie budget
+#ans8
+SELECT AVG(custo) FROM filmes;
+
+#q9: movies more expensive than mean value
+#ans9
+SELECT * FROM filmes WHERE custo > (SELECT AVG(custo) FROM filmes);
+
+#ans10
+SELECT * FROM filmes WHERE nota_especialistas > (SELECT AVG(nota_especialistas) FROM filmes);
+
+#ans11
+SELECT * FROM filmes WHERE nota_audiencia > (SELECT AVG(nota_audiencia) FROM filmes);
+
+#ans12
+SELECT * FROM filmes WHERE nota_audiencia > (SELECT AVG(nota_audiencia) FROM filmes) ORDER BY nota_audiencia ASC;
+
+#ans13
