23da658cf919f746b884489540bf8ea8dc83bb7f
Diógenes Wallis de França Silva <diogeneswallis@outlook.com>
2020-04-30 23:33:56 -0300

    Update movies.sql

--- questions/movies.sql
+++ questions/movies.sql
@@ -1,4 +1,4 @@
-#questions 1-4
+#questions
 
 #q1: top rated movie by normal people
 #ans1
@@ -15,3 +15,9 @@ SELECT * FROM filmes ORDER BY nota_audiencia ASC LIMIT 10;
 #q4: most hated movies by professionals
 #ans4
 SELECT * FROM filmes ORDER BY nota_especialistas ASC LIMIT 10;
+
+#q5: most expensive movie and most cheap movie
+#ans5
+SELECT * FROM filmes WHERE custo = (SELECT MAX(custo) FROM filmes);
+SELECT * FROM filmes WHERE custo = (SELECT MIN(custo) FROM filmes);
+
